074b70aa8c72abf7061a2b6b3f253c9470f154f5
M-Laetitia <molin.laetitia@gmail.com>
2024-12-05 21:08:23 +0100

    create route to get the categories

---
+++ app/api/category/route.ts
@@ -0,0 +1,16 @@
+import { db} from "@/lib/db"
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const categories = await db.category.findMany({
+            where: {
+                isPrimary: true,
+            },
+          });
+        return NextResponse.json(categories)
+    } catch (error) {
+        console.error("[CATEGORIES] Error:", error)
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
