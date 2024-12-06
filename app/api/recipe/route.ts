4ba5a4bb01134aed5fb5ccb26e6eabd3d8c9c6bd
M-Laetitia <molin.laetitia@gmail.com>
2024-12-06 14:08:01 +0100

    add a param to limit the nb of recipes

--- app/api/recipe/route.ts
+++ app/api/recipe/route.ts
@@ -2,13 +2,16 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-
-export async function GET() {
+export async function GET(req: Request) {
     // types de requête API : GET, POST, PUT, OPTIONS, PATCH, DELETE, HEAD, CONNECT, TRACE
+
+    const { searchParams } = new URL(req.url); //extraire les paramètres de la requête
+    const limit = searchParams.get("limit");  // Récupère le paramètre `limit` (ex. ?limit=5) / si limit est défini, la clause take est ajoutée pour limiter le nombre de résultats.
+
     try {
         const recipes = await db.recipe.findMany({
             orderBy: {
-                name: 'asc'
+                createdAt: 'desc' 
             },
             include: {
                 categories: {
@@ -16,26 +19,23 @@ export async function GET() {
                         category: true
                     }
                 },
-                // comments : true // récupérer tout ce qu'il y a dans commentaire
 
                 reviews: {
                     select: {
                         content: true  // Sélectionne uniquement le champ 'text'
                     }
                 }
-                
-            }
+            },
+            // Toujours inclure la base (comme 10) par précaution, même si ici : un paramètre de requête HTTP la chaîne sera très probablement déjà en base 10. Cela évite toute ambiguïté et est considéré comme une bonne pratique.
+            ...(limit && { take: parseInt(limit, 10) }) // Limite si `limit` est défini
         })
- 
 
-        // return une réponse au format Json
-        console.log("liste recette", recipes)
         return NextResponse.json(recipes)
 
     } catch (error) {
         // ce console log ne s'affiche pas dans le navigateur (pour cela il faudrait qu'on soit dans un composant client) or ici on est côté serveur mais dans le terminal dans lequel on est en train d'exécuter le projet 
         // SSR
-        console.log("[ARTICLES]", error)
+        console.log("[RECIPES]", error)
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
