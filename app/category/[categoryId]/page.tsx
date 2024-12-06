c5f8dca305894c092c4a3460e90562022ac45612
M-Laetitia <molin.laetitia@gmail.com>
2024-12-06 23:18:35 +0100

    Fix: correctly extract recipes from API response by mapping the data

--- app/category/[categoryId]/page.tsx
+++ app/category/[categoryId]/page.tsx
@@ -1,9 +1,17 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 type Recipe = {
   id: string;
   name: string;
+
+};
+
+type RecipeResponse = {
+  id: string;
+  recipeId: string;
+  categoryId: string;
+  recipe: Recipe;  // L'objet recette lui-même
 };
 
 type Category = {
@@ -17,27 +25,29 @@ type Props = {
 }
 
 const CategoryPage = ({ params }: Props) => {
-const [recipes, setRecipes] = useState<Category[]>([]);
-const [categoryId, setcategoryId] = useState<string | null>(null); 
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [categoryId, setcategoryId] = useState<string | null>(null); 
 
-useEffect(() => {
-  // Résoudre la promesse de params et extraire articleId
-  const fetchCategoryId = async () => {
-    const resolvedParams = await params;
-    setcategoryId(resolvedParams.categoryId); // Mettre à jour CategoryId
-  };
-  
-  fetchCategoryId();
-}, [params]); // Mettre à jour l'effet lorsque params change
+  useEffect(() => {
+    // Résoudre la promesse de params et extraire articleId
+    const fetchCategoryId = async () => {
+      const resolvedParams = await params;
+      setcategoryId(resolvedParams.categoryId); // Mettre à jour CategoryId
+    };
+    
+    fetchCategoryId();
+  }, [params]); // Mettre à jour l'effet lorsque params change
 
 
   useEffect(() => {
     const fetchRecipes = async () => {
       // Utiliser `categoryId` pour la requête
+            //  if (!categoryId) return;
       const response = await fetch(`/api/category/${categoryId}`);
       const data = await response.json();
-      console.log(data); 
-      setRecipes(data);
+      console.log("recipe categ", data); 
+      const recipes = data.map((item: RecipeResponse) => item.recipe); // Extraire la recette de chaque élément
+      setRecipes(recipes); // Mettre à jour l'état des recettes
     };
 
     if (categoryId) {
@@ -51,7 +61,7 @@ useEffect(() => {
 
   return (
     <div>
-    <h1>Recipe for this {categoryId}</h1>
+    <h1>Recipe for {categoryId}</h1>
     {recipes.length > 0 ? (
       recipes.map((recipe: Recipe) => (
         <div key={recipe.id}>
@@ -65,4 +75,5 @@ useEffect(() => {
   </div>
   );
 };
+
 export default CategoryPage;
