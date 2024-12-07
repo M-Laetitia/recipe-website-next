dd3e9649fc61a9b0a45fc86e856a1c77cf24ad27
M-Laetitia <molin.laetitia@gmail.com>
2024-12-07 18:36:42 +0100

    Style: add swiper for the categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-// import React from 'react' 
 'use client'
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import { getCldImageUrl } from 'next-cloudinary';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
+import { Navigation } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
 type Category = {
   id: string;
@@ -50,40 +50,122 @@ const Home = () => {
 
   return (
     <>
-      {/* SECTION 1 - image  */}
+      {/* SECTION 1 - HERO */}
       <section className="relative w-full flex-center flex-col" style={{ height: 'calc(100vh - 96px)' }}>
-          <div className='absolute z-20'>
-            <div>LOGO</div>
-            <p>NAME WEBSITE</p>
-          </div>
-          <div className='absolute w-full h-full bg-blackColor opacity-80 z-10'></div>
-          <figure className='w-full h-full relative overflow-hidden'>
-              <Image
-              src="/img/top-view-table-full-food.jpg"
-              alt="A table seen from above with different dishes"
-              layout="responsive"
-              width={1200} 
-              height={800}
-              objectFit="contain"
-              />
-          </figure>
+        <div className='absolute z-20'>
+          <div>LOGO</div>
+          <p>NAME WEBSITE</p>
+        </div>
+        <div className='absolute w-full h-full bg-blackColor opacity-80 z-10'></div>
+        <figure className='w-full h-full relative overflow-hidden'>
+            <Image
+            src="/img/top-view-table-full-food.jpg"
+            alt="A table seen from above with different dishes"
+            //intrinsic / fill
+            layout="responsive"
+            width={2000} 
+            height={1333}
+            // cover
+            objectFit="contain"
+            />
+        </figure>
       </section>
 
-      {/* SECTION 1 - categories  */}
-      <section className="w-full flex-center flex-col h-screen bg-purple-900">
-        {categories.length > 0 ? (
-          categories.map((category: Category) => (
-            // <div key={category.id}>{category.name}</div>
-            <div key={category.id}>
-              {/* Lien vers la page de la catégorie, avec l'id dans l'URL */}
-              <Link href={`/category/${category.id}`}>
-                {category.name}
-              </Link>
+  
+
+      {/* SECTION 1 - CATEGORIES */}
+      <section className="w-full flex flex-col  items-center h-screen bg-blackColor">
+        <div className='w-[80%] h-full pt-[8rem] px-10 flex flex-col items-center text-whiteColor'>
+          <h2 className="title-medium mb-5" >Recipes by category</h2>
+          <p className='text-small w-[70%] mb-24 text-center'>
+            Explore Recipes by Category - 
+            from savory snacks to indulgent desserts, our recipes cover it all. Find inspiration for every meal of the day and every occasion !
+          </p>
+
+          <div className='w-full flex '>
+            <div className='transform scale-x-[-1] flex items-center'>
+              <Image
+              src="/img/right-arrow-sketch.svg"
+              alt="A left arrow"
+              width={40} 
+              height={40}
+              />
             </div>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
+
+            <div className='w-full flex flex-row justify-evenly'>
+
+                {categories.length > 0 ? (
+                  <Swiper
+                  modules={[Navigation]}
+                  navigation={{
+                    prevEl: '/img/right-arrow-sketch.svg',
+                    nextEl: '/img/right-arrow-sketch.svg',
+                  }}
+                  spaceBetween={0}         // Espace entre les slides
+                  slidesPerView={4}        // Affiche 3 slides à la fois
+                  onSlideChange={() => console.log('slide change')}
+                  onSwiper={(swiper) => console.log(swiper)}
+                >
+                {categories.map((category: Category) => (
+                  <SwiperSlide key={category.id}>
+                <div className='bg-[#D98341] w-[215px] h-[270px]'>
+                  <div className='h-[80%] w-full p-4'>
+                    <Image className=''
+                        src={getCldImageUrl({
+                        src: 'starter_white_hlwzzf',
+                        width: 400,
+                        height: 400,
+                        crop: 'fit'
+                        })}
+                        alt='test'
+                        width={400}
+                        height={400}
+                    />
+                  </div>
+                  <p className='h-[20%] title-small text-center pt-4'>
+                    <Link href={`/category/${category.id}`}>
+                      {category.name}
+                    </Link>
+                  </p>
+                </div>
+                </SwiperSlide>
+                ))}
+                </Swiper>
+              ) : (
+                <p>Loading...</p>
+              )}
+
+            </div>
+            
+            <div className='flex items-center'>
+              <Image
+              src="/img/right-arrow-sketch.svg"
+              alt="A left arrow"
+              width={40} 
+              height={40}
+              />
+            </div>
+
+          </div>
+
+
+
+          {categories.length > 0 ? (
+            categories.map((category: Category) => (
+              // <div key={category.id}>{category.name}</div>
+              <div key={category.id}>
+                {/* Lien vers la page de la catégorie, avec l'id dans l'URL */}
+                <Link href={`/category/${category.id}`}>
+                  {category.name}
+                </Link>
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+
+
+        </div>
       </section>
 
       {/* SECTION 1 - latest recipes */}
