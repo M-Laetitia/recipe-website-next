b54c8c9215e9ef349b8fcee8f6dea386ecca56f2
M-Laetitia <molin.laetitia@gmail.com>
2024-12-08 14:54:02 +0100

    Style: add hover effect on the category cards + clean code

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,12 @@ type Recipe = {
   image: string;
 };
 
-
-
 const Home = () => {
   const [categories, setCategories] = useState([])
   const [recipes, setRecipes] = useState([])
   const colors = ['#D98341', '#EAEAEA', '#0B161A']
   const borderColors = ['#D98341', '#EAEAEA', '#D98341']
+  const fontColors = ['#EAEAEA', '#D98341', '#EAEAEA']
 
   useEffect (() => {
     const fetchCategories = async() => {
@@ -46,7 +45,6 @@ const Home = () => {
     fetchRecipes();
   }, [])
 
-
   // const url = getCldImageUrl({
   //   width: 960,
   //   height: 600,
@@ -61,7 +59,7 @@ const Home = () => {
           <div>LOGO</div>
           <p>NAME WEBSITE</p>
         </div>
-        <div className='absolute w-full h-full bg-blackColor opacity-80 z-10'></div>
+        <div className='absolute w-full h-full bg-[#0B161A] opacity-80 z-10'></div>
         <figure className='w-full h-full relative overflow-hidden'>
             <Image
             src="/img/top-view-table-full-food.jpg"
@@ -76,160 +74,156 @@ const Home = () => {
         </figure>
       </section>
 
-  
-
       {/* SECTION 1 - CATEGORIES */}
       <section className="w-full flex flex-col  items-center h-screen bg-blackColor">
-        <div className='w-[80%] h-full pt-[8rem] px-10 flex flex-col items-center text-whiteColor'>
-          <h2 className="title-medium mb-5" >Recipes by category</h2>
-          <p className='text-small w-[70%] mb-24 text-center'>
+        <div className='w-[80%] h-full pt-[5rem] px-10 flex flex-col items-center text-whiteColor'>
+          <div className='mb-8 relative flex'>
+            <Image
+              src="/img/paint_stroke.svg"
+              alt="paint Stroke"
+              width={200} 
+              height={45}
+              className="transform rotate-[-5deg]"
+              />
+            <p className='w-full text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursive-medium '>Some words ! </p>
+          </div>
+          <h2 className="title-medium mb-2">Recipes by category</h2>
+          <p className='text-small w-[70%] mb-20 text-center'>
             Explore Recipes by Category - 
             from savory snacks to indulgent desserts, our recipes cover it all. Find inspiration for every meal of the day and every occasion !
           </p>
 
           <div className='w-full flex '>
-            
-
             <div className='w-full flex flex-row justify-evenly'>
-
                 {categories.length > 0 ? (
                   <Swiper
-                  modules={[Navigation]}
-                  navigation={{
-                    prevEl: '.swiper-button-prev',
-                    nextEl: '.swiper-button-next',
-                  }}
-                  spaceBetween={10}         // Espace entre les slides
-                  slidesPerView={4}        // Affiche 3 slides à la fois
-                  onSlideChange={() => console.log('slide change')}
-                  onSwiper={(swiper) => console.log(swiper)}
-                >
-
-                <div className=' swiper-button-prev flex items-center rotate-180 '>
-                  <Image
-                  src="/img/right-arrow-sketch.svg"
-                  alt="Previous"
-                  width={50} 
-                  height={50}
-                  style={{ width: '50px', height: '50px', maxWidth: 'none' }}
-                  />
-                </div>
-           
-                <div className=' swiper-button-next flex items-center  w-[50px] h-[50px]'>
-                  <Image
-                  src="/img/right-arrow-sketch.svg"
-                  alt="Next"
-                  width={50} 
-                  height={50}
-                  style={{ width: '50px', height: '50px', maxWidth: 'none' }}
-                  />
-                </div>
-
-                {categories.map((category: Category,  index: number ) => (
-                  <SwiperSlide key={category.id}>
-                  <div 
-                    style={{
-                      backgroundColor: colors[index % colors.length],
-                      borderColor: borderColors[index % borderColors.length],
-                      borderWidth: '2px',
-                      borderStyle: 'solid' 
+                    modules={[Navigation]}
+                    navigation={{
+                      prevEl: '.swiper-button-prev',
+                      nextEl: '.swiper-button-next',
                     }}
-                    className='w-[215px] h-[270px]'>
-                  <div className='h-[80%] w-full p-4'>
-                    <Image className=''
-                        src={getCldImageUrl({
-                        src: category.image,
-                        width: 400,
-                        height: 400,
-                        crop: 'fit'
-                        })}
-                        alt='test'
-                        width={400}
-                        height={400}
-                    />
-                  </div>
-                  <p className='h-[20%] title-small text-center pt-4'>
-                    <Link href={`/category/${category.id}`}>
-                      {category.name}
-                    </Link>
-                  </p>
-                </div>
-                </SwiperSlide>
-                ))}
-                </Swiper>
-              ) : (
-                <p>Loading...</p>
-              )}
+                    spaceBetween={10}         // Espace entre les slides
+                    slidesPerView={4}        // Affiche 3 slides à la fois
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    >
 
+                    <div className=' swiper-button-prev flex items-center rotate-180 '>
+                      <Image
+                      src="/img/right-arrow-sketch.svg"
+                      alt="Previous"
+                      width={50} 
+                      height={50}
+                      style={{ width: '50px', height: '50px', maxWidth: 'none' }}
+                      />
+                    </div>
+
+                    <div className=' swiper-button-next flex items-center  w-[50px] h-[50px]'>
+                      <Image
+                      src="/img/right-arrow-sketch.svg"
+                      alt="Next"
+                      width={50} 
+                      height={50}
+                      style={{ width: '50px', height: '50px', maxWidth: 'none' }}
+                      />
+                    </div>
+                    
+                    {categories.map((category: Category,  index: number ) => (
+                      <SwiperSlide key={category.id}>
+                      <Link href={`/category/${category.id}`}>
+                        <div 
+                          style={{
+                            backgroundColor: colors[index % colors.length],
+                            borderColor: borderColors[index % borderColors.length],
+                            borderWidth: '2px',
+                            borderStyle: 'solid' 
+                          }}
+                          className='w-[215px] h-[270px] group'>
+
+                          <div className='h-[80%] w-full p-4 '>
+                            <Image className=''
+                                src={getCldImageUrl({
+                                src: category.image,
+                                width: 400,
+                                height: 400,
+                                crop: 'fit'
+                                })}
+                                alt='test'
+                                width={400}
+                                height={400}
+                            />
+                          </div>
+
+                          <div style={{ color: fontColors[index % fontColors.length] }}>
+                          {/* pt-4 */}
+                            <p className='h-[20%] title-small text-center pt-4 group-hover:pt-0 transition-all duration-300 '>
+                                {category.name}
+                            </p>
+                            <p className='text-center pt-2 group-hover:pt-0 transition-all duration-400'>{category.recipeCount} recipes</p>
+                          </div>
+                        </div>
+                      </Link>
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                ) : (
+                  <p>Loading...</p>
+                )}
             </div>
-            
-           
-
           </div>
-
-
         </div>
       </section>
 
       {/* SECTION 1 - latest recipes */}
       <section className="w-full flex-center h-screen flex justify-center">
-
-      <div className='w-full'>
-        <h1>Latest recipes :</h1>
-        <div className='w-4/5 bg-slate-500 flex flex-row'>
-
-          
-        {recipes.length > 0 ? (
-        <Swiper
-          spaceBetween={20}         // Espace entre les slides
-          slidesPerView={3}        // Affiche 3 slides à la fois
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          {recipes.map((recipe: Recipe) => (
-            <SwiperSlide key={recipe.id}>
-              <div className="w-[300px] h-[450px]">
-                <div>
-                <Image
-                    className="h-full w-full object-cover object-center"
-                    src={getCldImageUrl({
-                      src: recipe.image,
-                      width: 960,
-                      height: 600,
-                      crop: 'fit',
-                    })}
-                    alt={recipe.name}
-                    width={960}  
-                    height={600} 
-                  />
-                </div>
-                <p>
-                  <Link href={`/recipe/${recipe.id}`}>{recipe.name}</Link>
-                </p>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      ) : (
-        <p>Loading...</p>
-      )}
-
+        <div className='w-full'>
+          <h1>Latest recipes :</h1>
+          <div className='w-4/5 bg-slate-500 flex flex-row'>
+            {recipes.length > 0 ? (
+              <Swiper
+                spaceBetween={20}         // Espace entre les slides
+                slidesPerView={3}        // Affiche 3 slides à la fois
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+              >
+                {recipes.map((recipe: Recipe) => (
+                  <SwiperSlide key={recipe.id}>
+                    <div className="w-[300px] h-[450px]">
+                      <div>
+                      <Image
+                          className="h-full w-full object-cover object-center"
+                          src={getCldImageUrl({
+                            src: recipe.image,
+                            width: 960,
+                            height: 600,
+                            crop: 'fit',
+                          })}
+                          alt={recipe.name}
+                          width={960}  
+                          height={600} 
+                        />
+                      </div>
+                      <p>
+                        <Link href={`/recipe/${recipe.id}`}>{recipe.name}</Link>
+                      </p>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
         </div>
-      </div>
-
       </section>
-
       
       {/* SECTION 1 - actual theme  ? */}
       <section className="w-full flex-center h-screen flex-col">
-            
       </section>
 
       {/* SECTION 1 - latest articles  */}
       <section className="w-full flex-center flex-col h-screen bg-purple-900">
-        
       </section>
-
 
     </>
   )
