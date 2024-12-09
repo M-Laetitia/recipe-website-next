c342f500b7d7544e2dfbb343aa0d54563d515780
M-Laetitia <molin.laetitia@gmail.com>
2024-12-10 00:12:13 +0100

    Style: add logo to the navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
 // import { useState, useEffect } from 'react';
 // afficher style différent pour liens actifs
 import { usePathname } from 'next/navigation';
@@ -9,10 +10,58 @@ import ThemeToggle from '@/components/ThemeToggle';
 
 const Nav = () => {
     const currentPath = usePathname();
+    const links = [
+        { title: 'Profile', url: '/profile' },
+        { title: 'Dashboard', url: '/user' },
+        { title: 'Admin Dashboard', url: '/admin', role: 'admin' },
+        // Add more placeholder links as needed
+      ];
 
     return (
     <nav className='bg-blackColor w-full h-24 px-16 grid grid-cols-[auto_1fr_auto_auto] items-center justify-between josefin'>
-        <div className=''>LOGO</div>
+
+        <div className= 'w-[105px] h-[70px] flex items-center justify-center'>
+          <Image
+            src="/img/logo.svg"
+            alt="Just cook logo"
+            width={105} 
+            height={70}
+          />
+        </div>
+       
+
+        {/* user panel */}
+        {/* <div className='text-white'>
+            <Link href='/profile'>
+            <div className='mr-5 cursor-pointer hover:text-gray-900'>
+              Profile
+            </div>
+          </Link>
+          <Link href='/user'>
+            <div className='mr-5 cursor-pointer hover:text-gray-900'>
+              Dashboard
+            </div>
+          </Link>
+          <Link href='/admin'>
+            <div className='mr-5 cursor-pointer hover:text-gray-900'>
+              Admin Dashboard
+            </div>
+          </Link>
+        </div> */}
+
+          {/* login / sign up */}
+        {/* <div className='md:flex items-center'>
+          <a href='/sign-in'>
+            <button className='text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-base mr-4'>
+              Login
+            </button>
+          </a>
+          <a href='/sign-up'>
+            <button className='text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-base'>
+              Sign Up
+            </button>
+          </a>
+        </div> */}
 
         <div className='flex items-center justify-end space-x-6 mx-16'>
             <Link
