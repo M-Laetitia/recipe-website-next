d353d394acc58e57c55e21ba8faf0a0e9d391eae
M-Laetitia <molin.laetitia@gmail.com>
2024-12-09 23:30:51 +0100

    create signIn page (clerk)

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/nextjs';
+
+const SignInPage = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
+
