db312f2a484c813ee432bd7e19609f86390357ca
M-Laetitia <molin.laetitia@gmail.com>
2024-12-11 23:53:05 +0100

    create button component

---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+ 
+// Définition de l'interface pour les props
+interface ButtonProps {
+    text: string;
+  }
+
+// Utilisation de l'interface dans le composant
+const Button = ({ text }: ButtonProps) => {
+
+    return (
+        <div className='bg-accentColor inline-block px-6 py-3.5 font-medium text-base tracking-widest cursor-pointer hover:bg-[#E59B62] transition ease-in-out delay-150'>
+            {text}
+        </div>
+    )
+}
+
+export default Button
