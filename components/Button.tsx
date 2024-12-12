4a74c1cf1ca3a3b207f285a85aedccd17eff9e11
M-Laetitia <molin.laetitia@gmail.com>
2024-12-12 23:37:27 +0100

    Style: add className to the button props

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 // Définition de l'interface pour les props
 interface ButtonProps {
     text: string;
+    className : string; 
   }
 
 // Utilisation de l'interface dans le composant
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text , className }: ButtonProps) => {
 
     return (
-        <div className='bg-accentColor inline-block px-6 py-3.5 font-medium text-base tracking-widest cursor-pointer hover:bg-[#E59B62] transition ease-in-out delay-150'>
+        <div className={`btn ${className='bg-accentColor inline-block px-6 py-3.5 font-medium text-base tracking-widest cursor-pointer hover:bg-[#E59B62] transition ease-in-out delay-150'}`}>
             {text}
         </div>
     )
