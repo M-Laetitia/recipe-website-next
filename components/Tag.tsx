1552480f36756ae6a11b595eee6af26df6c6d6be
M-Laetitia <molin.laetitia@gmail.com>
2024-12-15 19:15:54 +0100

    Feat/style: create a tag component

---
+++ components/Tag.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+ 
+interface TagProps {
+    text: string;
+    bgColor : string; 
+  }
+
+const Tag = ({ text , bgColor}: TagProps) => {
+
+    return (
+         <div 
+            style={{ 
+                backgroundColor: bgColor,
+                transition: 'background-color 150ms ease-in-out'
+            }}
+            className='inline-block border border-solid border-accentColor px-3 py-2 font-medium text-small lowercase tracking-widest cursor-pointer'
+            onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#e56b62'}
+            onMouseOut={(e) => e.currentTarget.style.backgroundColor = bgColor}
+        >
+            {text}
+        </div>
+    )
+}
+
+export default Tag
