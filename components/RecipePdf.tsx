42092074a83d2f60b6de3593b2e8c66f77a7f774
M-Laetitia <molin.laetitia@gmail.com>
2024-12-19 23:09:19 +0100

    Feat: create RecipePdf component to generate PDF documents for recipes

---
+++ components/RecipePdf.tsx
@@ -0,0 +1,90 @@
+import { Document, Page, Text, View, PDFViewer, StyleSheet } from '@react-pdf/renderer';
+import React, { useEffect, useState } from 'react';
+
+interface Props {
+  recipeId: string;
+}
+
+
+
+const RecipePdf = ({ recipeId }: Props) => {
+  const [recipe, setRecipe] = useState<any | null>(null);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      const response = await fetch(`/api/recipe/${recipeId}`);
+      const data = await response.json();
+      setRecipe(data);
+      console.log("data pdf", data)
+    };
+
+    if (recipeId) {
+      fetchRecipe();
+    }
+  }, [recipeId]);
+
+  const styles = StyleSheet.create({
+    page: {
+      padding: 30,
+      fontFamily: 'Helvetica',
+    },
+    name: {
+      fontSize: 24,
+      marginBottom: 20,
+      textAlign: 'center',
+    },
+    section: {
+      marginBottom: 15,
+    },
+    header: {
+      fontSize: 18,
+      marginBottom: 10,
+    },
+    text: {
+      fontSize: 12,
+      lineHeight: 1.5,
+    },
+    ingredientList: {
+      marginTop: 10,
+    },
+    ingredient: {
+      fontSize: 12,
+      marginBottom: 5,
+    },
+  });
+
+  if (!recipe) {
+    return (
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <Text>loading...</Text>
+        </Page>
+      </Document>
+    );
+  }
+
+
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        {/* Titre */}
+        <Text style={styles.name}>{recipe.name}</Text>
+
+        {/* Ingrédients */}
+        <View style={styles.section}>
+            <Text style={styles.header}>Ingrédients :</Text>
+            {recipe.ingredients.map((ingredient: { ingredient: { name: string }; quantity: number; unit: string }, index: number) => (
+                <Text key={index} style={styles.ingredient}>
+                - {ingredient.ingredient.name} ({ingredient.quantity} {ingredient.unit})
+                </Text>
+            ))}
+        </View>
+
+        
+      </Page>
+    </Document>
+  );
+};
+
+export default RecipePdf;
