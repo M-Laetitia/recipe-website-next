aab047cb98809d2d563355b92c8ea999f129b263
M-Laetitia <molin.laetitia@gmail.com>
2025-01-10 16:19:13 +0100

    prettier clean code

--- components/SearchSuggestions.tsx
+++ components/SearchSuggestions.tsx
@@ -1,45 +1,56 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 interface SearchSuggestionsProps {
-    suggestions: { recipes: { id: string; name: string }[]; articles: { id: string; title: string }[] }; 
-    isLoading: boolean;
+  suggestions: {
+    recipes: { id: string; name: string }[];
+    articles: { id: string; title: string }[];
+  };
+  isLoading: boolean;
 }
 
 // const SearchSuggestions = ( suggestions , isLoading : SearchSuggestionsProps) => {
-    const SearchSuggestions: React.FC<SearchSuggestionsProps> = ({ suggestions, isLoading }) => {
+const SearchSuggestions: React.FC<SearchSuggestionsProps> = ({
+  suggestions,
+  isLoading,
+}) => {
+  if (isLoading) return <p>Loading...</p>;
+  if (suggestions.recipes.length === 0 && suggestions.articles.length === 0)
+    return <p>No results found</p>;
 
-    if (isLoading) return <p>Loading...</p>;
-    if (suggestions.recipes.length === 0 && suggestions.articles.length === 0) return <p>No results found</p>;
-
-    return (
-        <div className="absolute bg-white border mt-1 w-full max-w-xs shadow-lg z-50">
-          {suggestions.recipes.length > 0 && suggestions.recipes.map((suggestion) => (
-            <div key={suggestion.id} className="p-2">
-                <Link 
-                href={`/recipe/${suggestion.id}`} 
-                target="_blank" 
-                rel="noopener noreferrer"
-                >
-                {suggestion.name}
-                </Link>
-              
-            </div>
-          ))}
-    
-          {suggestions.articles.length > 0 && suggestions.articles.map((suggestion) => (
-            <div key={suggestion.id} className="p-2">
-                <Link 
-                    href={`/article/${suggestion.id}`}
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                    {suggestion.title}
-                </Link>
+  return (
+    <div className="absolute bg-white border mt-1 w-full max-w-xs shadow-lg z-50">
+        <p>Recipes : </p>
+      {suggestions.recipes.length > 0 &&
+        suggestions.recipes.map((suggestion) => (
+          <div key={suggestion.id} className="p-2">
             
+            <Link
+              href={`/recipe/${suggestion.id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {suggestion.name}
+            </Link>
           </div>
-          ))}
-        </div>
-      );
-    }
+        ))}
 
-export default SearchSuggestions
+        <p>Articles : </p>
+      {suggestions.articles.length > 0 &&
+        suggestions.articles.map((suggestion) => (
+          <div key={suggestion.id} className="p-2">
+            <p></p>
+            <Link
+              href={`/article/${suggestion.id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {suggestion.title}
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SearchSuggestions;
