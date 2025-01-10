a2a05f6b82dd66802d575ecbbf6224a5ecd68958
M-Laetitia <molin.laetitia@gmail.com>
2025-01-10 14:48:39 +0100

    Feat: add category filtering functionality to recipe page

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -4,10 +4,13 @@
 
 "use client"
 
-import RecipeCard2 from '@/components/RecipeCard2'
+
 // import Button from '@/components/Button'
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+
+import RecipeCard2 from '@/components/RecipeCard2'
+import FilterPanel from '@/components/FilterPanel'
 
 
 type Category = {
@@ -31,6 +34,9 @@ const RecipePage = () => {
 
     // VERSION 02 : HOOKS
     const [recipes, setRecipes] = useState([])
+    const [categories, setCategories] = useState([])
+    const [filteredCategory, setFilteredCategory] = useState(null);
+
 
     useEffect (() => {
         // je consomme l'api et je stocke  dans articles les résultats obtenus dans l'api
@@ -51,34 +57,62 @@ const RecipePage = () => {
         // différence entre tableau vide et ne rien mettre 
     }, [])
 
+    useEffect (() => {
+        const fetchCategories = async() => {
+            const response = await fetch('/api/category?main=true')
+            const data = await response.json()
+            setCategories(data)
+        }
+        fetchCategories();
+    }, [])
+
+    const handleFilterChange = (selectedCateg: string | null) => {
+
+        if (!selectedCateg) {
+            setFilteredCategory(null); // Réinitialiser si aucun filtre n'est sélectionné
+            return;
+          }
+    
+        const fetchFilteredCategory = async() => {
+            const response = await fetch(`/api/recipe?category=${selectedCateg}`);
+            const data = await response.json()
+            setFilteredCategory(data)
+            console.log('filteredCategory', data)
+        }
+        fetchFilteredCategory();
+        };
+
+        // Les recettes à afficher (par défaut ou filtrées)
+        const recipesToDisplay = filteredCategory || recipes;
+        console.log('recipesToDisplay', recipesToDisplay)
+
     return (
         // fragments
         <>
         {/* <Button label='Contact' href='/contact' /> */}
             <h1 className='text-4xl font-bold mb-6'>RECIPES</h1>
+            <FilterPanel
+                title="Main Category"
+                options={categories}
+                onFilterChange={handleFilterChange}
+            />
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {/* Liste des articles */}
                 {/* boucle en js à travers React dans un composant  */}
                 {/* articles > tableau  : article > 1 élément article de la BDD  / un document (mongoDB)*/}
-                {recipes.map((recipe: Recipe, index: number) =>(
-                    <RecipeCard2 key={index} recipe={recipe}/>
-                ))}
+
+                {recipesToDisplay.length === 0 ? (
+                    <p>
+                        No recipes found. Try adjusting your filters.
+                    </p>
+                ) : (
+                    recipesToDisplay.map((recipe: Recipe, index: number) => (
+                        <RecipeCard2 key={index} recipe={recipe} />
+                    ))
+                )}
             </div>
 
-            <div>
-            {recipes.map((recipe: Recipe,  index: number) =>(
-                    <div key={index}>
-                        <p >
-                        {/* <Link href={`/recipe/${recipe.id}`}> */}
-                        <Link href={`/recipe/${recipe.id}`}>
-                        {recipe.name}
-                        </Link>
-                        </p>
-                        
-                    </div>
-                   
-                ))}
-            </div>
+          
         </>
     )
 }
