126cbb9cdeca03afa4806f311c1fd35678313d1d
M-Laetitia <molin.laetitia@gmail.com>
2025-05-16 18:40:22 +0200

    fix

--- app/api/article/route.ts
+++ app/api/article/route.ts
@@ -1,19 +1,98 @@
-import { NextRequest } from "next/server";
+import { db } from "@/lib/db"
 import { NextResponse } from "next/server";
-import { db } from "@/lib/db";
+// import { ClerkExpressWithAuth } from "@clerk/express";
+import { clerkClient } from "@clerk/express"; 
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const { title, content, image } = body;
+export async function GET() {
+    try {
+        const articles = await db.article.findMany({
+            orderBy : {
+                createdAt: 'desc'
+            },
+            include: {
+                tags: {
+                    include: {
+                        tag: true
+                    }
+                },
+            }
+        })
 
-    const article = await db.article.create({
-      data: { title, content, image }
-    });
+        // 2. rajouter aux articles les infos de l'user depuis Clerk
+        // Promise.all est utilisé pour exécuter plusieurs promesses en parallèle. Cela permet de récupérer les informations utilisateur de Clerk pour chaque article en simultané, au lieu de le faire séquentiellement (un après l'autre).
+        const articleWithUser = await Promise.all(
+            // mpa pour itérer sur les articles : à chaque itération, une promesse asynchrone est retournée. Cela signifie que chaque article est associé à une promesse qui va récupérer l'utilisateur associé via clerkClient.users.getUser(article.userId).
+            articles.map(async (article) => {
+                try {
+                    // 3. Récupérer l'user de Clerk via userId
+                    const user = await clerkClient.users.getUser(article.userId); // Remplacer par l'ID user de Clerk
+                    // 4. Retourner l'article avec les données user
+                    return {
+                        ...article,
+                        user: {
+                            id: user.id,
+                            username: user.username, 
+                        }
+                    };
+                } catch (error) {
+                    console.log(`Erreur lors de la récupération de l'user pour l'article ${article.id}:`, error);
+                    return { ...article, user: null };
+                }
+            })
+        );
 
-    return NextResponse.json(article);
-  } catch (error) {
-    console.error("[ARTICLE CREATE] Error:", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
+        console.log('articles all', articleWithUser)
+        return NextResponse.json(articleWithUser)
+
+    } catch (error) {
+        console.log("[ARTICLES]", error)
+        return new NextResponse("Internal Error", {status: 500})
+    }
+};
+
+export async function POST(req: Request) {
+    try{
+        const body = await req.json();
+        const { title, content, image} = body;
+
+
+        // Validation des champs
+        if (!title || typeof title !== "string") {
+            return NextResponse.json({ message: "Invalid title article" }, { status: 400 });
+        }
+
+        if (!content || typeof content !== "string") {
+            return NextResponse.json({ message: "Invalid content article" }, { status: 400 });
+        }
+
+        if (!image || typeof image !== "string") {
+            return NextResponse.json({ message: "Invalid image article" }, { status: 400 });
+        }
+
+        // ajouter le slug 
+        const slug = title.toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]+/g, '');
+
+        const userId= 'user_2q5MoDx5nUHSVNwhbg7vrmJ5jAC';
+
+
+        // Création de l'article
+        const newArticle = await db.article.create({
+            data: { 
+                title, 
+                content, 
+                slug, 
+                userId,
+                image 
+            },
+        });
+
+        return NextResponse.json(newArticle, { status:201 });
+
+    } catch (error) {
+        console.log("Error", error);
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+};
--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -1,37 +1,22 @@
-import { db } from "@/lib/db"
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-// import { ClerkExpressWithAuth } from "@clerk/express";
-import { clerkClient } from "@clerk/express"; 
+import { db } from "@/lib/db";
 
-export async function POST(req: Request) {
-    try{
-        const body = await req.json();
-        const {content, articleId} = body;
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { content, articleId } = body;
 
-        // Validation des champs
-        if (!content || typeof content !== "string") {
-            return new NextResponse("Content is required", {status: 400})
-        }
-        if (!articleId || typeof articleId !== "string") {
-            return new NextResponse("Recipe ID is required", { status: 400 });
-        }
+    const comment = await db.comment.create({
+      data: {
+        content,
+        articleId
+      }
+    });
 
-        const userId= 'user_2q5MoDx5nUHSVNwhbg7vrmJ5jAC';
-
-        // Ajout de la review
-        const NewComment= await db.comment.create({
-            data: {
-                content,
-                userId,
-                article: {
-                    connect: { id: articleId }
-                }
-            }
-        });
-
-        return NextResponse.json(NewComment, { status:201 });
-    } catch (error) {
-        console.log("[COMMENT]", error)
-        return new NextResponse("Internal Error", {status: 500})
-    }
+    return NextResponse.json(comment);
+  } catch (error) {
+    console.error("[COMMENT] Error:", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
